f4fa8162abee396c7691cdc3e3c4b0e0dbd31f70
Ashutosh Verma <ashutoshrgnict@gmail.com>
2024-06-16 11:10:21 +0530

    chore: Update CORS configuration to allow requests from specific origin

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://saathi.vercel.app",
+    methods: ["GET", "POST"],
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
